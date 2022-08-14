5669312df16ebe7a4465e0db8e678edaadf57214
HaolongHuai <1612907625@qq.com>
2022-08-14 19:38:10 +0800

    finall

--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -1,9 +1,24 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="register">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    register() {
+      this.GStore.flashMessage = this.event.title + ' has been updated '
+      setTimeout(() => {
+        //After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+      this.$$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
--- src/views/event/EventRegisterView.vue
+++ src/views/event/EventRegisterView.vue
@@ -1,9 +1,25 @@
 <template>
   <p>Registration form here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    register() {
+      this.GStore.flashMessage =
+        'You are successfully register for ' + this.event.title
+      setTimeout(() => {
+        //After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+      this.$$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
